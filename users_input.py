647317aab0ced991874f23d71f5f3c74fc1c74cf
priscahkosgei <jepkogei2020@students.uonbi.ac.ke>
2024-03-13 14:52:18 -0700

    users_input.py

---
+++ users_input.py
@@ -0,0 +1,11 @@
+# Ask for user's name
+name = input("Please enter your name: ")
+
+# Ask for user's age
+age = input("Please enter your age: ")
+
+# Ask for user's location
+location = input("Please enter your location: ")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
